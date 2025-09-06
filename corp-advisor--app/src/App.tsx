46ffebc065b9847a88fe919037caad87a993e306
Yeyeong99 <gongsam99@gmail.com>
2025-09-06 19:50:38 +0900

    design: add responsive design

--- corp-advisor--app/src/App.tsx
+++ corp-advisor--app/src/App.tsx
@@ -1,21 +1,18 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-import Home from './Screens/HomeScreen';
+import Home from "./Screens/HomeScreen";
 import Report from "./Screens/ReportScreen";
 import Chatbot from "./Screens/ChatbotScreen";
 
 function App() {
-
-  return <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />       {/* 홈 페이지 */}
-        <Route path="/report" element={<Report />} /> {/* About 페이지 */}
-        <Route path="/chatbot" element={<Chatbot />} /> {/* Contact 페이지 */}
-      </Routes>
-    </Router>
-  </>;
+  return (<> < Router > <Routes>
+    <Route path="/" element={<Home />}/> {/* 홈 페이지 */}
+    <Route path="/report" element={<Report />}/> {/* About 페이지 */}
+    <Route path="/chatbot" element={<Chatbot />}/> {/* Contact 페이지 */}
+  </Routes>
+</Router>
+</>);
 }
 
 export default App;
--- corp-advisor--app/src/Screens/HomeScreen.tsx
+++ corp-advisor--app/src/Screens/HomeScreen.tsx
@@ -1,8 +1,36 @@
 import "../App.css";
+import {useMediaQuery} from "react-responsive";
+
+const isMobile = useMediaQuery({
+  maxWidth: 768,
+  ssrMatchMedia: () => ({matches: false})
+}
+as any);
+
+const isTablet = useMediaQuery({
+  minWidth: 769,
+  maxWidth: 1224,
+  ssrMatchMedia: () => ({matches: false})
+}
+as any);
+
+const isDesktop = useMediaQuery({
+  minWidth: 1225,
+  ssrMatchMedia: () => ({matches: false})
+}
+as any);
 
 function Home() {
+  const h2Class = isMobile
+    ? "text-base font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200"
+    : isTablet
+      ? "text-md font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200"
+      : "text-2xl font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200";
+
   return (<div className="flex flex-col justify-center align-center text-center">
-    <h1 className="text-5xl font-bold mb-5">Corp<span className="text-indigo-700">Advisor</span></h1>
+    <h1 className="text-5xl font-bold mb-5">
+      Corp<span className="text-indigo-700">Advisor</span>
+    </h1>
     <div className="intro p-6">
       <h5 className="text-md text-gray-700 leading-relaxed">
         안녕하세요! CorpAdvisor입니다. 👋
@@ -10,9 +38,7 @@ function Home() {
     </div>
     <div className="grid grid-cols-2 gap-3">
       <a href="/report" className="group my-3 px-4 py-3 bg-gray-100 rounded-lg hover:bg-gray-700 transition-transform transition-colors duration-200 transform hover:scale-105">
-        <h2 className="text-lg font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200">
-          기업 분석 보고서 생성
-        </h2>
+        <h2 className={h2Class}>기업 분석 보고서 생성</h2>
         <h5 className="text-md text-gray-700 group-hover:text-gray-100 transition-colors duration-200">
           기업 소개, 재무 분석, 성장 가능성을 담은
           <br/>
@@ -20,7 +46,12 @@ function Home() {
         </h5>
       </a>
       <a href="/chatbot" className="group my-3 px-4 py-3 bg-gray-100 rounded-lg hover:bg-gray-700 transition-transform transition-colors duration-200 transform hover:scale-105">
-        <h2 className="text-lg font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200">
+        <h2 className={isMobile
+            ? "text-base font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200"
+            : isTablet
+              ? "text-md font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200"
+              : ""
+}>
           금융 자문 챗봇
         </h2>
         <h5 className="text-md text-gray-700 group-hover:text-gray-100 transition-colors duration-200">
